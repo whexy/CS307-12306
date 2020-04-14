b18302203c8b8d6f0bc5e20321d29d89d228e1c3
Frankk-SigAnt <cymatics_fsa@163.com>
2020-04-14 22:46:47 +0800

    Added login API

--- api/user.py
+++ api/user.py
@@ -1,11 +1,14 @@
+import datetime
+
 from flask import request
+from flask_jwt_extended import create_access_token
 from flask_restful import Resource
 
 from model.Database import DBSession
 from model.models import User
 
 
-class UserApi(Resource):
+class SignupApi(Resource):
     def post(self):
         body = request.get_json()
         new_user = User(**body)
@@ -15,3 +18,19 @@ class UserApi(Resource):
         session.commit()
         print(body)  # DEBUG
         return 200
+
+
+class LoginApi(Resource):
+    def post(self):
+        body = request.get_json()
+        # user = User.query().filter_by(username=body.get('username')).first()
+        session = DBSession()
+        user = session.query(User).filter(User.username.has(username=body.get('username')))
+        if user is None:
+            return {'error': 'Username not found'}, 401
+        authorized = user.check_password(body.get('password'))
+        if not authorized:
+            return {'error': 'Invalid password'}, 401
+        expires = datetime.timedelta(days=1)
+        access_token = create_access_token(identity=str(user.username), expires_delta=expires)
+        return {'token': access_token}, 200
