a47363ee6c97fb14aefbbe2094b850a43460b9e5
Frankk-SigAnt <cymatics_fsa@163.com>
2020-04-15 00:09:29 +0800

    Updated user info API

--- api/user.py
+++ api/user.py
@@ -30,7 +30,7 @@ class LoginApi(Resource):
             return {'error': 'Username not found'}, 401
         authorized = user.check_password(body.get('password'))
         if not authorized:
-            return {'error': 'Invalid password'}, 401
+            return {'error': 'Wrong password'}, 401
         expires = datetime.timedelta(days=1)
         access_token = create_access_token(identity=str(user.user_id), expires_delta=expires)
         return {'token': access_token}, 200
@@ -47,5 +47,22 @@ class UserInfoApi(Resource):
         return user.to_dict(), 200
 
     @jwt_required
-    def post(self):
-        pass
+    def patch(self):
+        body = request.get_json()
+        user_id = get_jwt_identity()
+        session = DBSession()
+        user = session.query(User).filter(User.user_id == user_id).first()
+        if user is None:
+            return {'error': 'User not found'}, 404
+        authorized = user.check_password(body.get('password'))
+        if not authorized:
+            return {'error': 'Wrong password'}, 401
+        if user.username != body.get('username'):
+            # TODO Check username exists
+            user.username = body.get('username')
+        user.real_name = body.get('real_name')
+        user.email = body.get('email')
+        user.phone_number = body.get('phone_number')
+        if body.get('new_password'):
+            user.password = body.get('new_password')
+        session.commit()
