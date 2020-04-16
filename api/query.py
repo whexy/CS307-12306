30552e6046ec8012fc4b4d48033af52dfed8177e
Whexy <whexy@outlook.com>
2020-04-16 19:27:23 +0800

    feat: query APIv2

--- api/query.py
+++ api/query.py
@@ -32,3 +32,44 @@ class QueryApi(Resource):
         # print(train_id)
 
         return jsonify(result=resp)
+
+
+class QueryApiV2(Resource):
+    def get(self):
+        depart_place = request.args.get('from')
+        arrival_place = request.args.get('to')
+        SQL = '''
+        with station_table as
+         (
+             select station_name, district_name, city_name, station_id
+             from station
+                      join district on station.district_id = district.district_id
+                      join city on district.city_id = city.city_id
+         )
+select distinct i.train_id, train_name
+from interval i
+         join train on train.train_id = i.train_id
+where i.train_id in
+      (select train_id
+       from interval
+       where dep_station in (select station_id
+                             from station_table
+                             where station_name like '%{}%'
+                                or district_name like '%{}%'
+                                or city_name like '%{}%'))
+  and i.train_id in
+      (select train_id
+       from interval
+       where arv_station in (select station_id
+                             from station_table
+                             where station_name like '%{}%'
+                                or district_name like '%{}%'
+                                or city_name like '%{}%'))
+        '''.format(depart_place, depart_place, depart_place, arrival_place, arrival_place, arrival_place)
+
+        session = DBSession()
+        resp = []
+        sql_result = session.execute(SQL)
+        for result in sql_result:
+            resp.append({result[0]: result[1]})
+        return jsonify(result=resp)
--- api/route.py
+++ api/route.py
@@ -1,5 +1,5 @@
 from api.locate import GeoApi
-from api.query import QueryApi
+from api.query import QueryApi, QueryApiV2
 from api.user import UserInfoApi, SignupApi
 
 
@@ -8,3 +8,4 @@ def initialize_routes(api):
     api.add_resource(UserInfoApi, '/user')
     api.add_resource(GeoApi, '/geo')
     api.add_resource(QueryApi, '/query')
+    api.add_resource(QueryApiV2, '/query/v2')
