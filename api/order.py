ba4371d88e38b33caa06c0f21802ab06e9531272
Whexy <whexy@outlook.com>
2020-05-05 00:53:05 +0800

    feat: Purchase API Delete

--- api/order.py
+++ api/order.py
@@ -63,3 +63,56 @@ class OrderApi(Resource):
         session.commit()
         session.flush()
         return jsonify(code=0, result={'order_id': new_order.order_id})
+
+    def delete(self):
+        body = request.get_json()
+        order_id = body.get('order_id')
+        # The delete function is to set the order status "cancelled", set ticket available false,
+        # and released the occupied seat.
+        session = DBSession()
+        current_order: Order = session.query(Order).filter(Order.order_id == order_id).first()
+        current_order.order_status = "cancelled"
+        session.commit()
+        session.flush()
+        current_ticket: Ticket = session.query(Ticket).filter(Ticket.ticket_id == current_order.ticket_id).first()
+        current_ticket.available = False
+        session.commit()
+        session.flush()
+        first_interval = current_ticket.first_interval
+        last_interval = current_ticket.last_interval
+        current_seat: Seat = session.query(Seat).filter(Seat.seat_id == current_ticket.seat_id).first()
+        train_id = session.query(Train.train_id).join(Interval, Interval.train_id == Train.train_id).filter(
+            Interval.interval_id == first_interval).first()
+
+        # Here to release the seat
+        first_id = session.query(Interval.interval_id) \
+            .join(Train, Train.train_id == Interval.train_id) \
+            .filter(Train.train_id == train_id, Interval.prev_id == None) \
+            .first()[0]
+        successive_train = session.query(Interval.interval_id, Interval.next_id) \
+            .filter(Interval.interval_id == first_id) \
+            .cte(name='successive_train', recursive=True)
+        st_alias = aliased(successive_train, name='st')
+        i_alias = aliased(Interval, name='i')
+        successive_train_rec = successive_train.union_all(
+            session.query(i_alias.interval_id, i_alias.next_id)
+                .filter(i_alias.interval_id == st_alias.c.next_id)
+        )
+        interval_list = session.query(successive_train_rec.c.interval_id) \
+            .order_by(successive_train_rec.c.interval_id) \
+            .all()
+        index = 1
+        first_index, last_index = 0, 0
+        for interval in interval_list:
+            interval_id = interval[0]
+            if interval_id == first_interval:
+                first_index = index
+            if interval_id == last_interval:
+                last_index = index
+            index += 1
+        current_seat.occupied = bin(
+            int(current_seat.occupied, 2) & int('0' * (last_index - first_index + 1), 2) << (40 - last_index))[
+                                2:].zfill(40)
+        session.commit()
+        session.flush()
+        return dict(code=0, result="Delete Success")
--- model/Database.py
+++ model/Database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# engine = create_engine('postgresql://checker:123456@121.36.40.215/cs307', encoding="utf-8")
-engine = create_engine('postgresql://checker:123456@121.36.40.215/cs307', encoding="utf-8", echo=True)
+engine = create_engine('postgresql://checker:123456@121.36.40.215/cs307', encoding="utf-8")
+# engine = create_engine('postgresql://checker:123456@121.36.40.215/cs307', encoding="utf-8", echo=True)
 DBSession = sessionmaker(bind=engine)
