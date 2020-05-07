03b2b751476a9369f552c8742cdd3f1d17edbe4d
Whexy <whexy@outlook.com>
2020-05-07 16:30:01 +0800

    feat: Production Database Deployment

--- model/Database.py
+++ model/Database.py
@@ -2,5 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # engine = create_engine('postgresql://checker:123456@121.36.40.215/cs307', encoding="utf-8")
-engine = create_engine('postgresql://checker:123456@121.36.40.215/cs307', encoding="utf-8", echo=True)
+engine = create_engine(
+    'postgresql://checker:123456@121.36.40.215/cs307',
+    pool_recycle=7200,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+    encoding="utf-8")
 DBSession = sessionmaker(bind=engine)
